31811664af5a19315427a2ad8a771e3a002668ba
od41 <odafe41@gmail.com>
2025-11-07 10:30:12 +0000

    Task 0: Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <h1 className="text-2xl font-bold">
+          Your canvas is ready
+        </h1>
+        <h2 className="text-lg text-gray-500">
+          Start building your project!
+        </h2>
+      </div>
+    </>
+  )
+}
+
+export default App
